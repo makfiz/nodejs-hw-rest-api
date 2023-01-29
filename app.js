8ab4df038d9067c5c9520f03552675d886a99545
makfiz <99556605+makfiz@users.noreply.github.com>
2023-01-29 20:47:52 +0200

    up

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const cors = require('cors');
 
 const contactsRouter = require('./routes/api/contacts');
-const authRouter = require('./routes/api/auth');
+const authRouter = require('./routes/api/user');
 
 const app = express();
 
@@ -12,6 +12,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/avatars", express.static("public/avatars"));
 
 app.use('/api/contacts', contactsRouter);
 app.use('/api/users', authRouter);
